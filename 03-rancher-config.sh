6a3376dd7e0242d8da558aa3305ab290c42856fb
grahamnscp <grahamnscp@gmail.com>
2026-03-27 14:10:53 +0000

    added rancher config for suse-ai-lifecycle-manager ui extension

--- 03-rancher-config.sh
+++ 03-rancher-config.sh
@@ -163,6 +163,32 @@ spec:
   url: oci://registry.suse.com/ai/charts
 EOF
 
+# cattle-ui-plugin-system - suse-ai-lifecycle-manager operator
+# https://documentation.suse.com/suse-ai/1.0/html/AI-deployment/ai-alternative-deployments.html#ai-lifecycle-manager-clusterrepo-creating
+
+Log "Creating creating UI Extension Catalog and installing suse-ai-lifecycle-manager on rancher$NODENUM.."
+
+# install operator
+helm install suse-ai-operator oci://ghcr.io/suse/chart/suse-ai-operator \
+  -n suse-ai-operator-system --create-namespace \
+  --version 0.1.0
+
+# Install suse-ai-lifecycle-manager extension, created ui extension catalog and repo
+cat <<EOF | kubectl -f -  > /dev/null 2>&1
+apiVersion: ai-platform.suse.com/v1alpha1
+kind: InstallAIExtension
+metadata:
+  name: suseai
+spec:
+  helm:
+    name: suse-ai-lifecycle-manager
+    url: "oci://ghcr.io/suse/chart/suse-ai-lifecycle-manager"
+    version: "1.0.0"
+  extension:
+    name: suse-ai-lifecycle-manager
+    version: "1.0.0"
+EOF
+
 #
 echo "${BWhi}**************************************"
 echo "Rancher Manager is running at:"
